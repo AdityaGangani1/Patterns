c50afc3af4eae627f4d71054bb1ed9afeb89b91a
aditya <sungangani7272@gmail.com>
2024-07-23 11:16:28 +0530

    first commit

---
+++ 4.c
@@ -0,0 +1,14 @@
+1
+2 2 
+3 3 3
+
+void triangle(int n) {
+	// Write your code here
+  
+for(int i=0; i<n; i++){
+  for(int j=0; j<=i; j++){
+    cout<<i<<" ";
+  }
+  cout<<endl;
+}
+}
---
+++ 5.c
@@ -0,0 +1,17 @@
+* * *
+* *
+*
+
+void seeding(int n) {
+	// Write your code here.
+
+for(int i=0; i<n; i++){
+	for(int j=n; j>i; j--){
+		cout<<"* ";
+
+	}
+	cout<<endl;
+
+}
+
+}
